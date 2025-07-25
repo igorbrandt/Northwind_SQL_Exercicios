b3a6d20852c0368357192cff5f5c336a20215592
igorbrandt <igorbrandt95@hotmail.com>
2025-07-25 15:23:51 -0300

    :sparkles: resolvi primeira parte do exercício 015

--- exercicio_015.sql
+++ exercicio_015.sql
@@ -1,2 +1,26 @@
 -- 15. Identifique o pedido mais caro de cada cliente… 
 -- e também o segundo mais caro
+
+WITH orders_by_client AS (
+	SELECT 
+		o.customer_id,
+		od.order_id,
+		((od.unit_price * od.quantity) * (1 - od.discount)) AS order_value
+	FROM order_details od
+	JOIN orders o ON o.order_id = od.order_id
+	ORDER BY o.customer_id
+),
+
+orders_ranking AS (
+	SELECT
+		obc.customer_id, 
+		obc.order_id,
+		obc.order_value,
+		RANK() OVER(PARTITION BY obc.customer_id ORDER BY obc.order_value DESC) AS order_rank
+	FROM orders_by_client obc
+)
+
+-- pedido mais caro de cada cliente
+SELECT *
+FROM orders_ranking ok
+WHERE ok.order_rank = 1
